5109875310666dca98cbd3a5b4bf25ca9bf51c24
Lucas Alves <lucasacod@hotmail.com>
2024-05-04 01:14:40 -0300

    second commit

--- src/main/java/list/OperacoesBasicas/CarrinhoDeCompra.java
+++ src/main/java/list/OperacoesBasicas/CarrinhoDeCompra.java
@@ -1,4 +1,70 @@
 package main.java.list.OperacoesBasicas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarrinhoDeCompra {
+
+    private List<Item> itemList;
+
+    public CarrinhoDeCompra() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        itemList.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome){
+        List<Item> itensParaRemover = new ArrayList<>();
+        if (!itemList.isEmpty()) {
+        for(Item i : itemList){
+            if(i.getNome().equalsIgnoreCase(nome)){
+                itensParaRemover.add(i);
+            }
+        }
+        itemList.removeAll(itensParaRemover);
+    } else {
+        System.out.println("Nao existem itens na lista.");
+        }
+    }
+
+    public double calcularValorTotal(){
+        double soma = 0.0;
+
+        for(Item i : itemList){
+            double valor = i.getPreco() * i.getQuantidade();
+            soma += valor;
+        }
+        return soma;
+    }
+
+    public void exibirItens(){
+        if (!itemList.isEmpty()) {
+        System.out.println(itemList);
+    } else {
+            System.out.println("A lista esta vazia.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CarrinhoDeCompra carrinho = new CarrinhoDeCompra();
+
+        carrinho.adicionarItem("Leite", 20.0, 5);
+        carrinho.adicionarItem("Ovos", 10.0, 7);
+        carrinho.adicionarItem("Porrete", 5.0, 10);
+
+        System.out.println("Todos os itens: ");
+        carrinho.exibirItens();
+        System.out.println("Valor total: " + carrinho.calcularValorTotal());
+
+        carrinho.removerItem("Leite");
+
+        System.out.println(" ");
+        System.out.println("Após retirar todos o leite: ");
+        carrinho.exibirItens();
+        System.out.println("Valor total: " + carrinho.calcularValorTotal());
+
+    }
 }
--- src/main/java/list/OperacoesBasicas/Item.java
+++ src/main/java/list/OperacoesBasicas/Item.java
@@ -1,4 +1,47 @@
 package main.java.list.OperacoesBasicas;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Item {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Item(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Item: " +
+                "nome: " + nome +
+                ", preco: " + preco +
+                ", quantidade: " + quantidade;
+    }
 }
